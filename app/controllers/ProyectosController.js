5e5700eed9ba031abbcf76213eb796a9d8b00329
Antonio Ramos <tonyprr@gmail.com>
2014-03-26 06:36:39 -0500

    empresa y avance proyectos

---
+++ app/controllers/ProyectosController.js
@@ -0,0 +1,50 @@
+'use strict';
+
+define(['app'], function (app) {
+
+    var proyectosController = function ($scope, $rootScope, $stateParams, $filter, $http, $location) {
+        var appTitle = 'Proyectos';
+        $scope.appTitle = appTitle;
+        $scope.highlight = function (path) {
+            return $location.path().substr(0, path.length) == path;
+        };
+
+        $("#banner").backstretch("./img/banner/proyectos.jpg");
+        var $window = $(window).on('resize', function() {
+            if ($(window).width() <= 767) {
+                            $('#contenido').height(
+                                    ($(window).height() - $('#header').height() - $('#footer').height() - 20) / 2
+                            );    
+            } else {
+                            $('#contenido').height(
+                                    $(window).height() - $('#header').height() - $('#footer').height() - 50
+                            );    
+            }
+            $("#banner").backstretch("./img/banner/proyectos.jpg");
+        }).trigger('resize'); //on page load     
+        
+        var owl = $("#proyectos-carousel");
+        owl.owlCarousel({
+            items : 4, //10 items above 1000px browser width
+            itemsDesktop : [1000,3], //5 items between 1000px and 901px
+            itemsDesktopSmall : [900,2], // 3 items betweem 900px and 601px
+            itemsTablet: [600,2], //2 items between 600 and 0;
+            itemsMobile : [500, 1], // itemsMobile disabled - inherit from itemsTablet option
+            
+            slideSpeed : 800,
+            paginationSpeed : 3000,
+
+            pagination : true,
+            autoPlay : true,
+            
+            autoHeight : true
+
+        });
+
+        
+        
+    };
+
+    app.register.controller('ProyectosController', ['$scope', '$rootScope', '$stateParams', '$filter', '$http', '$location', proyectosController]);
+    
+});
